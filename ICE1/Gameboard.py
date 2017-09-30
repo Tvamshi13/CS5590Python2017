dba721eebbdcb4e4ce7eb3bbd9ac3377113ae3f1
Tvamshi13 <Tvamshi13@gmail.com>
2017-09-29 22:43:49 -0500

    Update Gameboard.py

--- ICE1/Gameboard.py
+++ ICE1/Gameboard.py
@@ -1,1 +1,9 @@
-
+import random
+randomNumber = random.randint(0,9)
+print("Please enter a number between 0 to 9")
+guessNumber = int(input())
+if(guessNumber>randomNumber):
+    print("Your guess is greater")
+elif(guessNumber==randomNumber):
+    print("Perfect guess!!")
+else: print("Your input is lesser")
